65a8150d596b2cc56709c25051611634be8ab718
faizan <faizansohail077@gmail.com>
2024-10-08 16:17:07 +0500

    testing husky

--- server/src/mock/public.mock.ts
+++ server/src/mock/public.mock.ts
@@ -1,6 +1,6 @@
 export const mock_create_user = {
     first_name: "first name",
     last_name: "last_name",
-    email: "faizan6@faizan.com",
+    email: "maazaasd2@faizan.com",
     password: "string",
 }
